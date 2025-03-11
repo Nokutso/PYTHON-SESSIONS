9df0f67df524fb2bb5cb8ebae0b996b1d3a583e0
Nokutso <shxronjack@gmail.com>
2025-03-11 11:43:41 +0200

    First commit

---
+++ greetingapp.py
@@ -0,0 +1,3 @@
+name = input("Your name: ")
+colour = input("Your favourite colour: ")
+print(f"Hello {name}!, your favorite color {colour} is awesome!")
--- index.py
+++ index.py
@@ -1,1 +1,2 @@
 print("fuck life")
+print("At times")
